1137befcd67e1d742bc601b961aaa5866606a4b9
Jordi Boggiano <j.boggiano@seld.be>
2012-11-04 16:55:03 +0100

    Add normalization of speed/angle over time

--- node.js
+++ node.js
@@ -1,4 +1,4 @@
-var Node = function (game, matrix, x, y, angle) {
+var Node = function (game, matrix, x, y, angle, speed) {
     this.game = game;
     this.matrix = matrix;
 
@@ -7,7 +7,9 @@ var Node = function (game, matrix, x, y, angle) {
     this.x = x;
     this.y = y;
 
-    this.speed = 20;
+    this.baseSpeed = 20;
+    this.speed = speed || this.baseSpeed;
+    this.baseAngle = deg2rad(90);
     this.angle = deg2rad(angle || 90);
     this.accel = 0;
 };
@@ -15,6 +17,20 @@ var Node = function (game, matrix, x, y, angle) {
 Node.prototype.tick = function (multiplier) {
     this.x += this.speed * Math.cos(this.angle) * multiplier;
     this.y += this.speed * Math.sin(this.angle) * multiplier;
+
+    // easing of angle/speed
+    if (this.angle !== this.baseAngle) {
+        this.angle += (this.baseAngle - this.angle) * 0.5 * multiplier;
+        if (Math.abs(this.angle - this.baseAngle) < 0.2) {
+            this.angle = this.baseAngle;
+        }
+    }
+    if (this.speed !== this.baseSpeed) {
+        this.speed += (this.baseSpeed - this.speed) * multiplier;
+        if (Math.abs(this.speed - this.baseSpeed) < 0.2) {
+            this.speed = this.baseSpeed;
+        }
+    }
 };
 
 Node.prototype.draw = function (ctx) {
